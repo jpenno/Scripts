fcdc9bfd465c97c5a4caea03a675f2d9622ae695
jp <jpenno123@hotmail.com>
2022-04-25 11:21:59 +1000

    mad it so you can pick branck of dwm to install

--- Install-dwm.sh
+++ Install-dwm.sh
@@ -5,5 +5,17 @@ cd .config
 git clone https://github.com/jpenno/dwm
 echo "cloned https://github.com/jpenno/dwm"
 cd dwm
+git branch
+echo "enter branch"
+read branch
+echo Change to $branch
+git checkout $branch
 cd dwm
+echo "Installing dwm"
 sudo make clean install
+echo "Installed dwm"
+cd ..
+cd dwmblocks
+echo "Installing dwmblocks"
+sudo make clean install
+echo "Installed dwmblocks"
