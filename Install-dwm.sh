197cfbbd4c7d73b2b9c944140d62f424f42b6ecf
jp <jpenno123@hotmail.com>
2022-04-25 10:04:52 +1000

    working on install-dwm script

--- Install-dwm.sh
+++ Install-dwm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Installing DWM into .config/dwm"
-cd Documents
+cd ~
+cd .config
 git clone https://github.com/jpenno/dwm
 echo "cloned https://github.com/jpenno/dwm"
+sudo make clean install
