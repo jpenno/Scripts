669c58a25d71a671fae7940aa7a7ebf06a876611
jp <jpenno123@hotmail.com>
2022-05-01 00:40:39 +1000

    make Install-ibus-jp.sh sett anthy input mode to Hiragana

--- Install-ibus-jp.sh
+++ Install-ibus-jp.sh
@@ -4,7 +4,8 @@ sudo pacman -S ibus ibus-anthy
 
 echo "ibus set set preload engines"
 gsettings set org.freedesktop.ibus.general preload-engines "['xkb:au::eng','anthy']"
+echo "set anthy input mode to Hiragana"
+gsettings set org.freedesktop.ibus.engine.anthy.common input-mode 0
 echo "ibus set coustom font"
 gsettings set org.freedesktop.ibus.panel use-custom-font true
 gsettings set org.freedesktop.ibus.panel custom-font "Fira Sans Regular 30"
-
