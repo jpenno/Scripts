60ee6c3f5a1d23ec3d3db87d84fc58c7353c93b6
jp <jpenno123@hotmail.com>
2022-04-30 12:00:27 +1000

    fix typo in install-ibus-jp.sh

--- Install-ibus-jp.sh
+++ Install-ibus-jp.sh
@@ -3,7 +3,8 @@ echo "install ibus from pacman"
 sudo pacman -S ibus ibus-anthy
 
 echo "ibus set set preload engines"
-gsettings set org.freedesktop.ibus.general preload-engines "['xkb:au::en','anthy']"
+gsettings set org.freedesktop.ibus.general preload-engines "['xkb:au::eng','anthy']"
 echo "ibus set coustom font"
 gsettings set org.freedesktop.ibus.panel use-custom-font true
 gsettings set org.freedesktop.ibus.panel custom-font "Fira Sans Regular 30"
+
