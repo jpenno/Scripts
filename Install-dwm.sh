acf0508fca2a0d027c4c9cf78ad9d61dc174f1be
jpenno <jpenno123@hotmail.com>
2022-06-20 19:19:04 +1000

    remove branch selection

--- Install-dwm.sh
+++ Install-dwm.sh
@@ -5,11 +5,11 @@ cd .config
 git clone https://github.com/jpenno/dwm
 echo "cloned https://github.com/jpenno/dwm"
 cd dwm
-git branch
-echo "enter branch"
-read branch
-echo Change to $branch
-git checkout $branch
+#git branch
+#echo "enter branch"
+#read branch
+echo getting dracula-testing branch
+git checkout dracula-testing
 cd dwm
 echo "Installing dwm"
 sudo make clean install
